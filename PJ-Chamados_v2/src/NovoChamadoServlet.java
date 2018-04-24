777d18e35defff56dbb729fc578747617b2d10fa
renanFaveri <renanFaveri01@gmail.com>
2018-04-24 19:27:19 -0300

    teste de conexão com o BD

--- PJ-Chamados_v2/src/NovoChamadoServlet.java
+++ PJ-Chamados_v2/src/NovoChamadoServlet.java
@@ -28,11 +28,33 @@ public class NovoChamadoServlet extends HttpServlet{
 	out.println("</h1>");
 	
 	out.println("<form method='POST'>");
+	
 	out.println("Nome: ");	
-	out.println("<input type='text' name='txtNome'>");
+	out.println("<input type='text' name='txtNome'><br>");
+	
 	out.println("Cidade: ");	
-	out.println("<input type='text' name='txtCidade'>");
+	out.println("<input type='text' name='txtCidade'><br>");
+
+	out.println("Data: ");	
+	out.println("<input type='text' name='txtData'><br>");
+	
+	out.println("Descrição:");
+	out.println("<textarea rows='4' cols='50' name='txtAreaDescricao'>  </textarea><br>");
+	
+	out.println("Sexo");
+	out.println("<input type='radio' name='radioSexo' value='Homem' > Homem" + 
+			" <input type='radio' name='radioSexo' value='Mulher'> Mulher<br>");
+	
+	out.println("Curso:");
+	out.println("<select name= 'selectCurso'>" + 
+			 " <option value='Técnico em Informatica'>Técnico em Informatica</option>" + 
+			 " <option value='Técnico em Tecnologia'>Técnico em Tecnologia</option>" + 
+			"</select>");
+	
 	out.println("<input type='submit' value='Cadastrar'>");
+	
+	
+	
 	out.println("</form>");
 	
 	out.println("</body>");
@@ -49,9 +71,13 @@ public class NovoChamadoServlet extends HttpServlet{
 			
 			String nome = request.getParameter("txtNome");
 			String cidade = request.getParameter("txtCidade");
+			String datax = request.getParameter("txtData");
+			String descricao = request.getParameter("txtAreaDescricao");
+			String sexo = request.getParameter("radioSexo");
+			String curso = request.getParameter("selectCurso");
 			
 			Class.forName("com.mysql.jdbc.Driver");
-			String SQL = "INSERT INTO tab_chamados(nome,cidade)VALUES(?,?)";
+			String SQL = "INSERT INTO tab_chamados(nome,cidade,descricao,datax,sexo,curso)VALUES(?,?,?,?,?,?)";
 			
 			try {
 			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/scc","root","");
@@ -59,6 +85,10 @@ public class NovoChamadoServlet extends HttpServlet{
 			
 			pstm.setString(1,nome);
 			pstm.setString(2,cidade);
+			pstm.setString(3,descricao);
+			pstm.setString(4,datax);
+			pstm.setString(5,sexo);
+			pstm.setString(6,curso);
 			
 			pstm.execute();
 			pstm.close();
