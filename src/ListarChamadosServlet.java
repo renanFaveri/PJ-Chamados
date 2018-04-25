335ed94cc100167b1686aaaa26ee5d0ed30a3d07
renanFaveri <renanFaveri01@gmail.com>
2018-04-24 21:27:13 -0300

    listar BD em uma tabela

--- src/ListarChamadosServlet.java
+++ src/ListarChamadosServlet.java
@@ -4,7 +4,9 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -18,12 +20,63 @@ public class ListarChamadosServlet extends HttpServlet {
 		
 		PrintWriter out = response.getWriter();
 		
+	try {	
+		
 try {
+	
+	String SQL = "Select * FROM tab_chamados";
 			Class.forName("com.mysql.jdbc.Driver");
-			
-			try {
 			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/scc","root","");
 			
+			Statement stn= conn.createStatement();
+			ResultSet rs = stn.executeQuery(SQL);
+			
+			out.println("<html>");
+			out.println("<head>");
+			out.println("<title>");
+			out.println("Sistema de Lista de Chamados");
+			out.println("</title>");
+			out.println("</head>");
+			out.println("<body>");
+			out.println("<h1>");
+			out.println("Lista de Chamados");
+			out.println("</h1>");
+			
+			out.println("<table width='100%' border='1'>");
+			out.println("<tr>");
+			out.println("<th>Nome</th>");
+			out.println("<th>Cidade</th>");
+			out.println("<th>Editar</th>");
+			out.println("<th>Excluir</th>");
+			out.println("<tr>");
+			
+			out.println("<table width='100%' border='1'>");
+			out.println("<tr>");
+			out.println("<th>Nome</th>");
+			out.println("<th>Cidade</th>");
+			out.println("<th>Editar</th>");
+			out.println("<th>Excluir</th>");
+			out.println("<tr>");
+			
+			while(rs.next()) {
+				
+				out.println("<table width='100%' border='1'>");
+				out.println("<td>"+rs.getString("nome")+"</td>");
+				out.println("<td>"+rs.getString("cidade")+"</td>");				
+				
+				out.println("<td>Editar</td>");
+				out.println("<td>Excluir</td>");
+				out.println("<tr>");				
+			}
+	
+			out.println("</table>");
+			out.println("</body>");
+			out.println("</html>");	
+			
+			stn.execute(SQL);
+			stn.close();
+			conn.close();
+		
 			}catch(SQLException e){
 				out.println("Erro de Conexão com o BD");
 			}
